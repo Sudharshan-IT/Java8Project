070ce06bfc1bba9fd3a77170523528898d2f5bf2
Sudharshan <sudharshanjavaguide@gmail.com>
2023-08-29 11:26:04 +0530

    Added Welcome Method PredicateDemo Class

--- src/main/java/com/yadav/predicate/PredicateDemo.java
+++ src/main/java/com/yadav/predicate/PredicateDemo.java
@@ -3,7 +3,9 @@ package com.yadav.predicate;
 import java.util.function.Predicate;
 
 public class PredicateDemo {
-	
+	public void welcome() {
+		System.out.println("Welcome To Java 8 Features");
+	}
 	public static void main(String[] args) {
 		
 	}
